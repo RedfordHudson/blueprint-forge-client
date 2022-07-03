1756038732ee557c6cf082a2819d68c7c36309d5
RedfordHudson <redfordfoxhudson@gmail.com>
2022-07-03 13:58:21 -0700

    primitive connection

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import ResourcesList from './components/component.resourceslist';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={callAPI}>Fetch</button>
+        <ResourcesList />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function callAPI() {
+  // const URL = 'http://localhost:3001';
+  const URL = 'https://database-streamline-server.herokuapp.com/';
+  fetch(URL+'resources')
+    .then(response => response.json())
+    .then(json => alert(JSON.stringify(json)));
+}
+
+export default App;
---
+++ src/components/component.resourceslist.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class ResourcesList extends React.Component { 
+    constructor(props) {
+        super(props);
+
+        this.getResources = this.getResources.bind(this);
+        this.getResourceElements = this.getResourceElements.bind(this);
+        this.changeName = this.changeName.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            URL: 'https://database-streamline-server.herokuapp.com/resources',
+            resources: [],
+            name: ''
+        }
+    }
+
+    componentDidMount() {
+        this.getResources();
+    }
+
+    getResources() {
+        axios.get(this.state.URL)
+            .then((resourceData) => {
+                this.setState({
+                    resources:resourceData.data.map(resource => resource.name)
+                });
+            });
+    }
+
+    getResourceElements() {
+        return this.state.resources.map(resource => {
+            return <li key={resource}>{resource}</li>
+        })
+    }
+
+    changeName(e) {
+        this.setState({
+            name:e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const resource = {
+            name:this.state.name
+        }
+
+        console.log(resource);
+
+        axios.post(this.state.URL+'/add',resource)
+            .then(() => {
+                this.getResources();
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>{this.getResourceElements()}</ul>
+                <form onSubmit={this.onSubmit}>
+                    <input type='text'
+                        onChange={this.changeName} />
+                    <input type='submit'/>
+                </form>
+            </div>
+        )
+    }
+}
